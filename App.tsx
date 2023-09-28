cad2bfacab039944b6fe35aa933caf8401f9967c
GiaHuySE <tonghuy214@gmail.com>
2023-09-28 18:06:14 +0700

    delete AppLoader

--- App.tsx
+++ App.tsx
@@ -96,20 +96,12 @@ const App: React.FC = () => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
     const bootstrapAsync = async () => {
       let userToken;
 
       try {
         userToken = await AsyncStorage.getItem('logged-in');
-      } catch (e) {
-        // Restoring token failed
-      }
-
-      // After restoring token, we may need to validate it in production apps
-
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
+      } catch (e) {}
       dispatch({type: 'RESTORE_TOKEN', token: userToken as any});
     };
 
@@ -119,35 +111,23 @@ const App: React.FC = () => {
   const authContext = useMemo(
     () => ({
       signIn: async (data: any) => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `SecureStore`
-        // In the example, we'll use a dummy token
         let userToken;
 
         try {
           userToken = await AsyncStorage.getItem('logged-in');
           console.log(userToken);
-        } catch (e) {
-          // Restoring token failed
-        }
+        } catch (e) {}
 
         dispatch({type: 'SIGN_IN', token: userToken as string});
       },
       signOut: () => dispatch({type: 'SIGN_OUT'}),
       signUp: async (data: any) => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `SecureStore`
-        // In the example, we'll use a dummy token
         let userToken;
 
         try {
           userToken = await AsyncStorage.getItem('logged-in');
           console.log(userToken);
-        } catch (e) {
-          // Restoring token failed
-        }
+        } catch (e) {}
         dispatch({type: 'SIGN_IN', token: userToken as string});
       },
     }),
